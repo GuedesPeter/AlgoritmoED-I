e87a471df32c1f459398d55e2ee817b33571f898
Paulo Eduardo <eduardopeter91@gmail.com>
2023-04-25 10:57:25 -0300

    Trabalho 01 - Orientação a Objetos

---
+++ Trabalhos/OrientacaoObjetos/main.py
@@ -0,0 +1,13 @@
+from Desktop import Desktop
+from Notebook import Notebook
+
+
+desktop = Desktop()
+#desktop.getInformacoes()
+#desktop.cadastrar("Samsung", "Branco", "700W")
+#desktop.getInformacoes()
+
+notebook = Notebook()
+#notebook.getInformacoes()
+#notebook.cadastrar("Dell", "Preto", "15hs")
+#notebook.getInformacoes()
